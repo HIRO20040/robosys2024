373522bca6b0dab4372f8c7d27cd494bf3b6d01b
Hiroaki Ohmatsu <hizzino0817@gmail.com>
2024-10-31 12:46:10 +0900

    動作確認

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Hiroaki Ohmatsu 　　　　　
 # SPDX-License-Identifier: GPL-3.0-only
 
@@ -11,7 +11,23 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out=15
 [ "${out}" = 15.0 ] || ng "$LINENO"
++ '['15 = 15 ' ]'
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]        || ng "$LINENO"
+[ "${out}" = "" ]   || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]        || ng "$LINENO"
+[ "${out}" = "" ]   || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK 
++ '[' 0 = 0 ']'
++ echo OK
+OK
 exit $res
++ exit 0
